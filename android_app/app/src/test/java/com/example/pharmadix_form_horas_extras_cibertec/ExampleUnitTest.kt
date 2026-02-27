d74303fe863a62ddd810bc6bde5712f855806c2d
Jose Montero <josemontero2415@gmail.com>
2026-02-27 08:38:56 -0500

    feat: add native android module structure

---
+++ android_app/app/src/test/java/com/example/pharmadix_form_horas_extras_cibertec/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.pharmadix_form_horas_extras_cibertec
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
---
+++ android_app/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pharmadix_Form_Horas_Extras_Cibertec"
+include(":app")
